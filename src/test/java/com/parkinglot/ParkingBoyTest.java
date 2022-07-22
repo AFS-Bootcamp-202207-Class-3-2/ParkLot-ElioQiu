4ff294d6f36b19dd55a242c76b1167764d9d2076
blank1024 <13380497335@163.com>
2022-07-23 00:00:49 +0800

    test: add test return_Unrecognized_parking_ticket_when_fetch_given_a_parking_boy_with_two_parkingLots_and_used_parking_ticket

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -54,6 +54,22 @@ public class ParkingBoyTest {
         assertNotNull(car);
     }
 
+    @Test
+    void should_return_right_car_when_fetch_car_twice_given_a_parking_boy_with_two_parkingLots_and_two_parkedCars_and_two_tickets() {
+        //given
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(new ParkingLot(1));
+        parkingLotList.add(new ParkingLot(1));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        Ticket ticket1 = parkingBoy.park(new Car(1));
+        Ticket ticket2 = parkingBoy.park(new Car(2));
+        //when
+        Car car1 = parkingBoy.fetch(ticket1);
+        Car car2 = parkingBoy.fetch(ticket2);
+        //then
+        assertEquals(car1, new Car(1));
+        assertEquals(car2, new Car(2));
+    }
 
     @Test
     void should_return_No_available_position_when_fetch_given_a_parking_boy_with_full_parkingLot_and_a_car() {
@@ -90,20 +106,19 @@ public class ParkingBoyTest {
     }
 
     @Test
-    void should_return_right_car_when_fetch_car_twice_given_a_parking_boy_with_two_parkingLots_and_two_parkedCars_and_two_tickets() {
-        //given
+    void should_return_Unrecognized_parking_ticket_when_fetch_given_a_parking_boy_with_two_parkingLots_and_used_parking_ticket() {
         List<ParkingLot> parkingLotList = new ArrayList<>();
         parkingLotList.add(new ParkingLot(1));
         parkingLotList.add(new ParkingLot(1));
         ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
-        Ticket ticket1 = parkingBoy.park(new Car(1));
-        Ticket ticket2 = parkingBoy.park(new Car(2));
+        parkingBoy.park(new Car(1));
+        parkingBoy.park(new Car(2));
+        parkingBoy.fetch(new Ticket(1));
+        Ticket ticket = new Ticket(1);
         //when
-        Car car1 = parkingBoy.fetch(ticket1);
-        Car car2 = parkingBoy.fetch(ticket2);
+        UnrecognizedParkingTicketException exception = assertThrows(UnrecognizedParkingTicketException.class,
+                () -> parkingBoy.fetch(ticket));
         //then
-        assertEquals(car1, new Car(1));
-        assertEquals(car2, new Car(2));
+        assertEquals(Constant.UNRECOGNIZED_PARKING_TICKET, exception.getMessage());
     }
-    
 }
