aeab162eec1132c02bf3c7d88d2f29a94420a805
blank1024 <13380497335@163.com>
2022-07-23 12:51:29 +0800

    test: add testcase return_car_in_more_rate_parkingLot_when_park_given_a_smart_parkingBoy_with_two_parkingLot_and_second_more_rate_positions_and_a_car

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -1,5 +1,6 @@
 package com.parkinglot;
 
+import com.parkinglot.Strategy.SmartStrategy;
 import com.parkinglot.Strategy.SuperSmartStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,4 +30,16 @@ public class SuperSmartParkingBoyTest {
         boolean isParkedCarInFirstParkingLot = parkingBoy.getParkingLotList().get(0).isCarInThisLot(ticket);
         assertEquals(isParkedCarInFirstParkingLot, true);
     }
+
+    @Test
+    void should_return_car_in_more_rate_parkingLot_when_park_given_a_smart_parkingBoy_with_two_parkingLot_and_second_more_rate_positions_and_a_car() {
+        //given
+        parkingBoy.park(new Car(1));
+        //when
+        Ticket ticket = parkingBoy.park(new Car(2));
+        //then
+        boolean isParkedCarInSecondParkingLot = parkingBoy.getParkingLotList().get(1).isCarInThisLot(ticket);
+        assertEquals(isParkedCarInSecondParkingLot, true);
+    }
+
 }
